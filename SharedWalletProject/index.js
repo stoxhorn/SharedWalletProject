aed17d5813e2b91514f3dba5d63faebcdebf96fa
stoxhorn <33036381+stoxhorn@users.noreply.github.com>
2023-07-30 16:35:33 +0200

    Add files via upload

--- SharedWalletProject/index.js
+++ SharedWalletProject/index.js
@@ -1,33 +1,32 @@
 const contractAddress = "0xda2c26d29daa8277c05fbc0a86f0452f61c21f7d";
 
 var walletContract;
-
 const connect = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const walletContract = new ethers.Contract(contractAddress, abi, signer);
+    walletContract = new ethers.Contract(contractAddress, abi, signer);
     const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
     });
     const account = accounts[0];
-    
+  
     document.getElementById("wallet-address").textContent =
       account.substring(0, 5) + "..." + account.substring(37, 42);
     document.getElementById("pending-allowance").textContent =
       await walletContract.myAllowance();
     document.getElementById("wallet-balance").textContent =
       await walletContract.getWalletBalance();
-};
-
-const renew = async () => {
+  };
+  
+  const renew = async () => {
     const account = document.getElementById("user").value;
     const amount = document.getElementById("allowance").value;
     console.log("Account is: ", account);
     var tx = await walletContract.renewAllowance(account, amount, 86400);
     await tx.wait();
   };
-
-const spend = async () => {
+  
+  const spend = async () => {
     const account = document.getElementById("receiver").value;
     const amount = document.getElementById("amount").value;
     var tx = await walletContract.spendCoins(account, amount);
